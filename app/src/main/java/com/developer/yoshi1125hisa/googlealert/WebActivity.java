a417b58a5830430dcbb3520dde1f50a1acc996c9
yoshi <32868028+yoshi1125hisa@users.noreply.github.com>
2018-06-28 22:36:17 +0900

    APK file and images for application releases and images, screenshots added. Also added a function to save links from WebView to clipboard.

--- app/src/main/java/com/developer/yoshi1125hisa/googlealert/WebActivity.java
+++ app/src/main/java/com/developer/yoshi1125hisa/googlealert/WebActivity.java
@@ -1,6 +1,9 @@
 package com.developer.yoshi1125hisa.googlealert;
 
 import android.annotation.SuppressLint;
+import android.content.ClipData;
+import android.content.ClipDescription;
+import android.content.ClipboardManager;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,8 +12,14 @@ import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
+
 
 public class WebActivity extends AppCompatActivity {
+
+
+    String getUrl;
+
     @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +27,10 @@ public class WebActivity extends AppCompatActivity {
         setContentView(R.layout.activity_web);
 
 
+
+
         Intent intent = this.getIntent();
-     String getUrl = intent.getStringExtra("URL");
+        getUrl = intent.getStringExtra("URL");
         //レイアウトで指定したWebViewのIDを指定する。
         WebView  webView = (WebView)findViewById(R.id.webView);
 
@@ -42,5 +53,30 @@ public class WebActivity extends AppCompatActivity {
 
 
     }
+
+    public void clip(View v){
+
+        //クリップボードに格納するItemを作成
+        ClipData.Item item = new ClipData.Item(getUrl);
+
+//MIMETYPEの作成
+        String[] mimeType = new String[1];
+        mimeType[0] = ClipDescription.MIMETYPE_TEXT_URILIST;
+
+//クリップボードに格納するClipDataオブジェクトの作成
+        ClipData cd = new ClipData(new ClipDescription("text_data", mimeType), item);
+
+//クリップボードにデータを格納
+        ClipboardManager cm = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+        cm.setPrimaryClip(cd);
+
+
+        Toast.makeText(this, "クリップボードにコピーしました。", Toast.LENGTH_SHORT).show();
+
+
+
+    }
+
+
 
 }
