f5dd2ebc0fb5d5933e3a6571ba01a218cce7d179
yoshi <32868028+yoshi1125hisa@users.noreply.github.com>
2018-12-01 20:44:25 +0900

    [Update] Loading... を追加

--- app/src/main/java/com/developer/yoshi1125hisa/googlealert/QrActivity.java
+++ app/src/main/java/com/developer/yoshi1125hisa/googlealert/QrActivity.java
@@ -2,13 +2,10 @@ package com.developer.yoshi1125hisa.googlealert;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.hardware.display.DisplayManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.AndroidRuntimeException;
 import android.util.DisplayMetrics;
-import android.view.Display;
-import android.view.WindowManager;
 import android.widget.ImageView;
 
 import com.google.zxing.BarcodeFormat;
@@ -22,16 +19,11 @@ public class QrActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qr);
 
-
-
         Intent intent = this.getIntent();
         String getUrl = intent.getStringExtra("URL");
 
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
-
-
-
 
         int winW = dm.widthPixels;
         int winH = dm.heightPixels;
@@ -43,9 +35,8 @@ public class QrActivity extends AppCompatActivity {
         }else{
             size = winH;
         }
-//QRコード画像の大きさを指定(pixel)
 
-
+        //QRコード画像の大きさを指定(pixel)
 
         try {
             BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
@@ -53,7 +44,7 @@ public class QrActivity extends AppCompatActivity {
             Bitmap bitmap = barcodeEncoder.encodeBitmap(getUrl, BarcodeFormat.QR_CODE, size, size);
 
             //作成したQRコードを画面上に配置
-            ImageView imageViewQrCode = (ImageView) findViewById(R.id.imageView);
+            ImageView imageViewQrCode = findViewById(R.id.imageView);
             imageViewQrCode.setImageBitmap(bitmap);
 
         } catch (WriterException e) {
