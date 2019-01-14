6a9a3c6fd05440cf65d5e04ae5b458b765bead80
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-01-14 14:46:07 +0900

    [Del] Web files.

--- Web/files/script/screen-transition.js
+++
@@ -1,8 +0,0 @@
-// ローディング中の画面チラツキ防止
-$(window).on("load", function() {
-    $("body")
-        .css("visibility","initial")
-        .css("overflow", "initial")
-        .hide()
-        .fadeIn("slow");
-});
